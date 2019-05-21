32b24ada696f415b1c30f5dff279d56c3e986e9b
gerayzade <hgarayzada@gmail.com>
2019-05-21 22:11:21 +0400

    preload images when needed

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { cloneDeep } from 'lodash';
 // data
 import data from '../quotes.json';
 
@@ -34,7 +35,7 @@ export default class Quote extends React.Component {
     const mergeObjects = (a, b) => { return {...a, ...b} };
     const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
     // merge objects and shuffle them
-    const quotes = data.quotes.map(quote => mergeObjects(quote, { image: requireImage(quote.scene, quote.character) }));
+    const quotes = data.quotes.map(quote => mergeObjects(quote, { image: { src: requireImage(quote.scene, quote.character), loaded: false } }));
     // set initial state
     this.state = { 
       quotes: shuffleArray(quotes),
@@ -46,16 +47,18 @@ export default class Quote extends React.Component {
     // show quote after .15ms
     setTimeout(() => this.setState({ show: true }), 150);
   }
+  getNextQuoteId = (id) => id === this.state.quotes.length - 1 ? 0 : id + 1;
+  getPrevQuoteId = (id) => id === 0 ? this.state.quotes.length - 1 : id - 1;
   switchToggle = () => this.setState({ toggle: !this.state.toggle });
   switchLang = (lang) => this.setState({ lang: lang });
   switchQuote = (next = true) => {
+    const newId = next ? this.getNextQuoteId(this.state.id) : this.getPrevQuoteId(this.state.id);
     // hide current quote
     this.setState({ show: false });
-    // define next/prev quote id
-    const nextId = this.state.id === this.state.quotes.length - 1 ? 0 : this.state.id + 1;
-    const prevId = this.state.id === 0 ? this.state.quotes.length - 1 : this.state.id - 1;
+    // preload images
+    this.handleImagesPreload(newId);
     // show next/prev quote
-    setTimeout(() => this.setState({ id: next ? nextId : prevId, show: true }), 300);
+    setTimeout(() => this.setState({ id: newId, show: true }), 300);
   }
   handleKeyPress = (event) => {
     switch(event.key) {
@@ -63,6 +66,25 @@ export default class Quote extends React.Component {
       case 'ArrowLeft': this.switchQuote(false); break;
       case 'ArrowRight': this.switchQuote(true); break;
     }
+  }
+  handleImagesPreload = (id) => {
+    let quotes = cloneDeep(this.state.quotes);
+    // preload images for current/prev/next quotes
+    quotes = quotes.map((quote, i) => {
+      if(!quote.image.loaded && [id, this.getNextQuoteId(id), this.getPrevQuoteId(id)].indexOf(i) !== -1) {
+        let img = new Image();
+        img.src = quote.image.src;
+        quote.image.loaded = true;
+      }
+      return quote;
+    });
+    // update info about loaded images
+    if(quotes !== this.state.quotes)
+      this.setState({ quotes: quotes });
+  }
+  componentWillMount = () => {
+    // preload image for the first and its adjoined quotes
+    this.handleImagesPreload(this.state.id);
   }
   componentDidMount = () => {
     document.addEventListener('keydown', this.handleKeyPress);
@@ -72,7 +94,7 @@ export default class Quote extends React.Component {
     // render current quote
     return (
       <div className={`character-bg ${this.state.show ? 'show' : 'hide'}`} 
-        style={{ backgroundImage: `url(${this.quote.image})` }} onKeyPress={this.handleKeyPress}>
+        style={{ backgroundImage: `url(${this.quote.image.src})` }} onKeyPress={this.handleKeyPress}>
         <nav className="head-nav">
           <Link to="/about">About</Link>
           <div className={`lang-switch ${this.state.toggle ? 'toggle' : ''}`}>
